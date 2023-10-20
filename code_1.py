530dff762d8dc9f6c00fc45c5d29e939f56c8c41
webtangen <143550134+webtangen@users.noreply.github.com>
2023-10-20 22:47:39 +0300

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,27 +1,29 @@
-def generate_random_data():
-import random
+    });
+    for (let i = 0; i < 10; i++) {
+}
+        console.log("Random Number: " + item);
+}
 
 
+    data.forEach(item => {
+    return data;
+    const data = generateRandomData();
+function generateRandomData() {
+    }
+
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+main();    main()
     for item in data:
     data = [random.randint(1, 100) for _ in range(10)]
+import random
+    data = generate_random_data()
+        print(f"Random Number: {item}")
 def main():
-
+if __name__ == "__main__":
+def generate_random_data():
 
     return data
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-    data = generate_random_data()
-    main()    return data
-def main():
-import random
-    for item in data:
-if __name__ == "__main__":
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def generate_random_data():
-        print(f"Random Number: {item}")
 
 
-
-    main()
