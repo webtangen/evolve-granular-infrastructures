322dece8a324b251c1b13af9a11c0fbad8780181
webtangen <143550134+webtangen@users.noreply.github.com>
2023-10-21 23:55:13 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,55 @@
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+        print(f"Random Number: {item}")
+    for item in data:
+
+
+if __name__ == "__main__":
+import random
+    return data
+def main():
+def generate_random_data():
+    data = generate_random_data()
+    });
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+main();
+        console.log("Random Number: " + item);
+}
+function generateRandomData() {
+
+}
+    const data = [];
+    return data;
+    }
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+
+    const data = generateRandomData();
+
+    main()
+def generate_random_data():
+
+    data = generate_random_data()
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+def main():
+    return data
+import random
+        print(f"Random Number: {item}")
+public class RandomDataGenerator {
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+        for (int item : data) {
+
+}
+            System.out.println("Random Number: " + item);
+        }
+    public static void main(String[] args) {
