b456fdfc7717f4f838eab6a56908e22e0799328c
webtangen <143550134+webtangen@users.noreply.github.com>
2023-10-27 05:36:35 +0300

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,13 +1,14 @@
-public class RandomDataGenerator {
-        }
-        }
-        for (int i = 0; i < 10; i++) {
-    public static void main(String[] args) {
+        print(f"Random Number: {item}")
 
-            System.out.println("Random Number: " + item);
-        int[] data = new int[10];
-}
+def main():
+    return data
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
 
-        for (int item : data) {
-    }
-            data[i] = (int) (Math.random() * 100) + 1;
+
+def generate_random_data():
+    for item in data:
+import random
+
+    data = generate_random_data()
