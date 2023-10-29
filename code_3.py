483839b07707799d639d58a1223e6cf919b770be
webtangen <143550134+webtangen@users.noreply.github.com>
2023-10-29 06:51:54 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,55 +1,52 @@
+if __name__ == "__main__":
+def main():
         print(f"Random Number: {item}")
-
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def generate_random_data():
     return data
+
+
     for item in data:
 
-    main()
-def generate_random_data():
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
-def main():
 import random
-    data = generate_random_data()
-if __name__ == "__main__":        print(f"Random Number: {item}")
-def generate_random_data():
-if __name__ == "__main__":
-    return data
-def main():
+
     main()
+def main():
+
+
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    main()
+    return data
+    data = generate_random_data()
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():
+import random
+    main()
+if __name__ == "__main__":
+import random
+def main():
+        print(f"Random Number: {item}")
+
+def generate_random_data():
     for item in data:
 
 
-
-    data = generate_random_data()
     data = [random.randint(1, 100) for _ in range(10)]
-
-import random
+    data = generate_random_data()
+    return data    for item in data:
         print(f"Random Number: {item}")
+def generate_random_data():
 
+if __name__ == "__main__":
 
     data = generate_random_data()
-if __name__ == "__main__":
     data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
     return data
 
-    main()
 import random
 def main():
-    for item in data:    data.forEach(item => {
-    return data;
-    }
-}
-        data.push(Math.floor(Math.random() * 100) + 1);
-    for (let i = 0; i < 10; i++) {
-    const data = [];
-    });
-main();
-        console.log("Random Number: " + item);
-
-
-    const data = generateRandomData();
-}
-function generateRandomData() {
-function main() {
+    main()
