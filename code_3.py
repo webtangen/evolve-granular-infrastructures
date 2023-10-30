fcfe059b214b63978cbd2a3d5365bf3c9c96ec26
webtangen <143550134+webtangen@users.noreply.github.com>
2023-10-30 08:00:55 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,52 +1,42 @@
-if __name__ == "__main__":
-def main():
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
     data = generate_random_data()
-def generate_random_data():
-    return data
-
-
-    for item in data:
-
 import random
-
-    main()
-def main():
-
-
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-    main()
-    return data
-    data = generate_random_data()
-    for item in data:
     data = [random.randint(1, 100) for _ in range(10)]
 
-def generate_random_data():
-import random
-    main()
-if __name__ == "__main__":
-import random
-def main():
-        print(f"Random Number: {item}")
 
+
+    return data
+    main()
 def generate_random_data():
     for item in data:
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-    return data    for item in data:
-        print(f"Random Number: {item}")
-def generate_random_data():
-
+def main():
 if __name__ == "__main__":
 
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
+        print(f"Random Number: {item}")    const data = generateRandomData();
+    return data;
+}
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+    data.forEach(item => {
 
-import random
-def main():
-    main()
+    const data = [];
+}
+        console.log("Random Number: " + item);
+    }
+
+main();
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+}
+
+
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+    }
+        }
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        int[] data = new int[10];
+    public static void main(String[] args) {
+public class RandomDataGenerator {
