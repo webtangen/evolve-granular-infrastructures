90ab7c683d2c9c105c2c2090bc1aba0c87b8af9f
webtangen <143550134+webtangen@users.noreply.github.com>
2023-10-31 09:10:21 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,42 +1,54 @@
-    data = generate_random_data()
-import random
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+    public static void main(String[] args) {
+
+        int[] data = new int[10];
+        }
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+}
+        }
+    }
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        }
+
+
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+        }
+}
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {        print(f"Random Number: {item}")
+    for item in data:
+
     data = [random.randint(1, 100) for _ in range(10)]
 
-
-
     return data
-    main()
-def generate_random_data():
-    for item in data:
-def main():
+    data = generate_random_data()
+import random
 if __name__ == "__main__":
+def generate_random_data():
 
-        print(f"Random Number: {item}")    const data = generateRandomData();
-    return data;
-}
+
+def main():
+    main()    data.forEach(item => {
+
+
+    for (let i = 0; i < 10; i++) {
+    }
+main();
 function main() {
         data.push(Math.floor(Math.random() * 100) + 1);
-    });
-    data.forEach(item => {
-
     const data = [];
+    const data = generateRandomData();
+    return data;
 }
+    });
+}
+
         console.log("Random Number: " + item);
-    }
-
-main();
 function generateRandomData() {
-    for (let i = 0; i < 10; i++) {
-}
-
-
-            System.out.println("Random Number: " + item);
-        for (int i = 0; i < 10; i++) {
-    }
-        }
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-        int[] data = new int[10];
-    public static void main(String[] args) {
-public class RandomDataGenerator {
