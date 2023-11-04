cca11b4cd8abcf6f5500e10720cecda14319a2d8
webtangen <143550134+webtangen@users.noreply.github.com>
2023-11-04 13:45:30 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,14 +1,51 @@
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+    }
+}
+        }
+        }
+
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+def main():
+    for item in data:
+    data = generate_random_data()
         print(f"Random Number: {item}")
 
-def main():
-    return data
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
 
 
 def generate_random_data():
-    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
 import random
+    return data
+if __name__ == "__main__":
+    main()
+if __name__ == "__main__":
 
+def generate_random_data():
+
+import random
     data = generate_random_data()
+        print(f"Random Number: {item}")
+
+
+    for item in data:
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    return data    }
+    public static void main(String[] args) {
+}
+
+
+        for (int item : data) {
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+        }
+        }
